2a1f2d1c916102f4bdf7212442709a115bc8a1d4
JubyKim <uon9816@naver.com>
2022-06-24 22:26:37 +0900

    project start

---
+++ oneDayoneCut/oneDayoneCut/oneDayoneCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneDayoneCutApp.swift
+//  oneDayoneCut
+//
+//  Created by JUEUN KIM on 2022/06/24.
+//
+
+import SwiftUI
+
+@main
+struct oneDayoneCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
