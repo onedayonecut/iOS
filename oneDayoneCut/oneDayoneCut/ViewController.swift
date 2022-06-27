0a4be0c0ee789087efae7801a6dd2a3fb0c5bbac
JubyKim <uon9816@naver.com>
2022-06-27 15:49:46 +0900

    make project again

---
+++ oneDayoneCut/oneDayoneCut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  oneDayoneCut
+//
+//  Created by JUEUN KIM on 2022/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
